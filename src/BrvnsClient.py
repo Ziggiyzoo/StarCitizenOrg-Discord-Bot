9711f646d120dfec81b9e17897601108d7b612f5
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-16 20:04:31 +0100

    Modified project structure

---
+++ src/BrvnsClient.py
@@ -0,0 +1,36 @@
+"""
+BRVNS Discord Client Object
+"""
+
+from discord import Client
+
+
+class BrvnsClient(Client):
+    """
+    BRVNS Discord Client
+    """
+
+    async def on_ready(self):
+        """
+        Run when a User logs in
+
+        :raises TypeError
+        """
+
+        print('We have logged in as {0.user}'.format(self))
+
+    async def on_message(self, message):
+        """
+        Run when a message is received
+
+        :param message: The message object
+        :type message:
+        :return:
+        :rtype:
+        """
+
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
