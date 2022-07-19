22dec2d4b23363d9db7b8604a6244cdeef97b16f
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-20 00:53:09 +0100

    Improve Environment Variable validation;

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from src.brvns_client import BrvnsClient
 # Main Method
 if __name__ == "__main__":
     # Check for required environment variables
-    if "TOKEN" not in os.environ:
+    if "TOKEN" not in os.environ or os.environ['TOKEN'] == "":
         raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
 
     client: BrvnsClient = BrvnsClient()
