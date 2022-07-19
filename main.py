2a3fd57cbab485ee5014265e80bd1b1110960943
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-19 23:44:10 +0100

    Add README; Remove Replit; Correct Lint;
    - .breakpoints / .replit / replit.nix
      - Deleted files.
    
    - .github/workflows/verify_commit.yml
      - Corrected installation and execution of Pylint.
      - Add stage for attempting to start the application.
    
    - README.md
      - Added a README file with instructions for how to run the application.
    
    - main.py
      - Add check for required Environment Variable.
    
    - pyproject.toml
      - Updated Pylint, and moved into Dev Dependencies.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ BRVNS Discord Bot
 """
 
 import os
+
 from src.brvns_client import BrvnsClient
 
 # Main Method
 if __name__ == "__main__":
-    client: BrvnsClient = BrvnsClient()
+    # Check for required environment variables
+    if "TOKEN" not in os.environ:
+        raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
 
+    client: BrvnsClient = BrvnsClient()
     client.run(os.environ['TOKEN'])
