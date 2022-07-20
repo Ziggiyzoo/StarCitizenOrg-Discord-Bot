6080b04ecf8929ca6a37077fe1a7c49f6bd5f1f7
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-20 01:58:16 +0100

    Correct Secret Name;

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 BRVNS Discord Bot
 """
 
-import os
+from os import environ
 
 from src.brvns_client import BrvnsClient
 
 # Main Method
 if __name__ == "__main__":
     # Check for required environment variables
-    if "TOKEN" not in os.environ or os.environ['TOKEN'] == "":
-        raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
+    # if "TOKEN" not in environ or environ['TOKEN'] == "":
+    #     raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
 
     client: BrvnsClient = BrvnsClient()
-    client.run(os.environ['TOKEN'])
+    client.run(environ['TOKEN'])
