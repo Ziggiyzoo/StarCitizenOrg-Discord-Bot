e57efce63037c0034266d0a8f0d4172e17faceff
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-20 02:18:13 +0100

    Disable Token content test;

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from src.brvns_client import BrvnsClient
 # Main Method
 if __name__ == "__main__":
     # Check for required environment variables
-    if "TOKEN" not in environ or environ['TOKEN'] == "":
-        raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
+    # if "TOKEN" not in environ or environ['TOKEN'] == "":
+    #     raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
 
     client: BrvnsClient = BrvnsClient()
     client.run(environ['TOKEN'])
