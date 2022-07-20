699422ecda4eb12c7885bb32340ee10d778200ca
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-20 03:21:22 +0100

    Add readme remove replit (#3)
    * Add README; Remove Replit; Correct Lint;
    
    - .breakpoints / .replit / replit.nix
      - Deleted files.
    
    - .github/workflows/verify_commit.yml
      - Corrected installation and execution of Pylint.
    
    - README.md
      - Added a README file with instructions for how to run the application.
    
    - pyproject.toml
      - Updated Pylint, and moved into Dev Dependencies.
    
    * Add README; Remove Replit; Correct Lint;
    
    - .breakpoints / .replit / replit.nix
      - Deleted files.
    
    - .github/workflows/verify_commit.yml
      - Corrected installation and execution of Pylint.
      - Add stage for attempting to start the application.
    
    - README.md
      - Added a README file with instructions for how to run the application.
    
    - main.py
      - Add check for required Environment Variable.
    
    - pyproject.toml
      - Updated Pylint, and moved into Dev Dependencies.
    
    * Correct max Python version
    
    * Add Token to Workflow;
    
    * Improve Environment Variable validation;
    
    * Correct Token Name;
    
    * Correct Secret Name;
    
    * Correct Secret Name;
    
    * Correct Secret Name;
    
    * Re-enable Token content test;
    
    * Disable Token content test;
    
    * Re-enable Token content test;
    
    * Temporarily attempt to display token;
    
    * Temporarily attempt to display token;
    
    * Temporarily attempt to display token;
    
    * Remove display token;

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 BRVNS Discord Bot
 """
 
-import os
+from os import environ
+
 from src.brvns_client import BrvnsClient
 
 # Main Method
 if __name__ == "__main__":
-    client: BrvnsClient = BrvnsClient()
+    # Check for required environment variables
+    if "TOKEN" not in environ or environ['TOKEN'] == "":
+        raise ValueError("No value for Environment Variable 'TOKEN' supplied. Exiting...")
 
-    client.run(os.environ['TOKEN'])
+    client: BrvnsClient = BrvnsClient()
+    client.run(environ['TOKEN'])
