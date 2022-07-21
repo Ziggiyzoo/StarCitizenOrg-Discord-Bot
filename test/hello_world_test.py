0d8ddd6ecfa4133c4eacdd379fad05e797dd5cbb
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-21 02:37:24 +0100

    Add Unit Test Framework; (#4)
    * Add Unit Test Framework;
    
    - .github/workflows/verify_commit.yml
      - Add new Step to run Unit Tests.
    
    - test/hello_world_test.py
      - Add example Unit Test to validate that Testing Framework is operational.
    
    * Modify Python versions; Remove deps;
    
    * Update Python version reqs;
    
    * Change GH Aciton Job name;
    
    * Modify Workflow Python version;

---
+++ test/hello_world_test.py
@@ -0,0 +1,23 @@
+"""
+Test to verify that the test framework is running.
+"""
+
+import unittest
+
+
+class HelloWorldTestCase(unittest.TestCase):
+    """
+    Hello World Testcase Class
+    """
+    def test_hello_world(self):
+        """
+        Testcase to validate that the Tests are running
+        """
+        expected: str = "Hello, World!"
+        actual: str = "Hello, World!"
+
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
