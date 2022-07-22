9169215f2c179ffc80c7474caf69191399f79331
David Symons <davidsymons1999@gmail.com>
2022-07-22 03:44:15 +0100

    Can I sleep? Tests passing locally. Testing Lint

--- src/brvns_bot_logic.py
+++ src/brvns_bot_logic.py
@@ -2,21 +2,21 @@
 BRVNS Discord Bot Logic
 """
 
-from src.brvns_config import BrvnsConfig
+from src.brvns_resources import BrvnsResources
 
 
 class BrvnsLogic():
     """
     BRVNS Bot Logic
     """
-    brvns_config = BrvnsConfig()
+    brvns_config = BrvnsResources()
 
     def signup_string(self, author_name):
         """
         Return signup string
         """
-        message_content = self.brvns_config.get_config("StringsSection", "string.signup_string")
-        signup_string = f"Hello {author_name}. {message_content}"
+        message_content = self.brvns_config.get_config("STRINGS", "string.signup_string")
+        signup_string = f'Hello {author_name}. {message_content}'
         return str(signup_string)
 
     def add_extra_public_method(self):
--- test/signup_string_test.py
+++ test/signup_string_test.py
@@ -4,7 +4,7 @@ Test to verify that the test framework is running.
 
 import unittest
 from src.brvns_bot_logic import BrvnsLogic
-from src.brvns_config import BrvnsConfig
+from src.brvns_resources import BrvnsResources
 
 
 
@@ -12,15 +12,15 @@ class SignupStringTestCase(unittest.TestCase):
     """
     Signup String Testcase Class
     """
-    brvns_config = BrvnsConfig()
+    brvns_resources = BrvnsResources()
     brvns_logic = BrvnsLogic()
 
     def test_signup_string(self):
         """
         Testcase to validate that the correct string format is returned when sign up command is used
         """
-        expected: str = self.brvns_config.get_config("StringsSection", "string.test_signup_string")
-        actual: str = self.brvns_logic.signup_string("Ziggiyzoo")
+        expected = self.brvns_resources.get_config("STRINGS", "string.test_signup_string")
+        actual = self.brvns_logic.signup_string("Ziggiyzoo")
 
         self.assertEqual(actual, expected)
 
