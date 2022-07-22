adab973e363eb4344d166a6cf0f9bb7cb5578762
David Symons <davidsymons1999@gmail.com>
2022-07-22 01:09:47 +0100

    Add in ConfigFile and attempt to satisfy pylint..

--- test/signup_string_test.py
+++ test/signup_string_test.py
@@ -4,19 +4,22 @@ Test to verify that the test framework is running.
 
 import unittest
 from src.brvns_bot_logic import BrvnsLogic
+from src.brvns_config import BrvnsConfig
+
 
 
 class SignupStringTestCase(unittest.TestCase):
     """
     Signup String Testcase Class
     """
+    brvns_config = BrvnsConfig()
+
     def test_signup_string(self):
         """
         Testcase to validate that the correct string format is returned when sign up command is used
         """
-        expected_string = "Hello Ziggiyzoo. "
-        expected_string += "To signup to the Blue Ravens Org, visit: https://robertsspaceindustries.com/orgs/BRVNS."
-        expected: str = expected_string
+        message_content = self.brvns_config.get_config("StringsSection", "string.signup_string")
+        expected: str = f"Hello Ziggiyzoo. {message_content}"
         actual: str = BrvnsLogic.signup_string("Ziggiyzoo")
 
         self.assertEqual(actual, expected)
