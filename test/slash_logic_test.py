a2d84e41efa344ff4e057b7a0fe709246d54633e
David S <davidsymons1999@gmail.com>
2023-03-03 18:15:30 +0000

    Address test failures.

--- test/slash_logic_test.py
+++ test/slash_logic_test.py
@@ -12,14 +12,14 @@ class SlashLogicTestCase(unittest.TestCase):
     Signup String Testcase Class
     """
 
-    def test_signup_string(self):
+    async def test_signup_string(self):
         """
         Testcase to validate that the correct string format is returned when sign up command is used
         """
         expected = resources_logic.get_string(
             "TEST_EXPECTED", "string.test_signup_string", testing=True
         )
-        actual = slash_logic.signup_string("Ziggiyzoo")
+        actual = await slash_logic.signup_string("Ziggiyzoo")
 
         self.assertEqual(actual, expected)
 
