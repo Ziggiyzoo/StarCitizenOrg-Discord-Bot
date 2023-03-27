7461333ae9b74672b0113726011b8dd9d1471a24
Renegade-Master <renegade@renegade-master.com>
2023-03-27 22:10:24 +0100

    Correct FB Cred path;

--- src/logic/__init__.py
+++ src/logic/__init__.py
@@ -8,4 +8,4 @@ def _get_firebase_secret_path() -> str:
     if os.name == "nt":
         return os.path.join(os.getenv("APPDATA"), "\\secrets\\firebase_secret.json")
 
-    return os.path.dirname("/var/secrets/firebase_secret.json")
+    return os.path.join("/var/secrets/firebase_secret.json")
